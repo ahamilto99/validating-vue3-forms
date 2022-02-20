dc6e8241a1bd6a4ac2c58fc948c9e71694f254b3
ahamilto99 <ajhamilton15@hotmail.com>
2022-02-19 19:43:27 -0500

    Completed Lesson 4 - Validating at the Form Level

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -2,23 +2,27 @@
   <form @submit.prevent="onSubmit">
     <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
 
-    <BaseInput label="Password" type="password" />
+    <BaseInput
+      label="Password"
+      type="password"
+      v-model="password"
+      :error="passwordError"
+    />
 
     <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
   </form>
 </template>
 
 <script>
-import { useField } from 'vee-validate'
+import { useField, useForm } from 'vee-validate'
 export default {
   setup() {
     function onSubmit() {
       alert('Submitted')
     }
 
-    const { value: email, errorMessage: emailError } = useField(
-      'email',
-      function(value) {
+    const validations = {
+      email: value => {
         if (!value) {
           return 'This field is required'
         }
@@ -27,13 +31,32 @@ export default {
           return 'Please enter a valid email address'
         }
         return true
+      },
+      password: value => {
+        const requiredMessage = 'This field is required'
+        if (value === undefined || value === null) {
+          return requiredMessage
+        }
+        // cannot be empty
+        if (!String(value).length) {
+          return requiredMessage
+        }
+        return true
       }
+    }
+    useForm({ validationSchema: validations })
+
+    const { value: email, errorMessage: emailError } = useField('email')
+    const { value: password, errorMessage: passwordError } = useField(
+      'password'
     )
 
     return {
       onSubmit,
-      email: email,
-      emailError: emailError
+      email,
+      emailError,
+      password,
+      passwordError
     }
   }
 }
